651fb05b163659f40b34b12861649869617d737e
Mel_The_Idiot <melvinngoi123@gmail.com>
2025-12-21 17:02:40 +0800

    Update player_vars.tres, player_animations.gd, p_2000.gd, and 10 more files

--- Scripts/player_animations.gd
+++ Scripts/player_animations.gd
@@ -2,22 +2,21 @@ extends Node3D
 
 @export var skeleton : Skeleton3D
 @export var camera_point : Node3D
+@export var body_point : Node3D
 @export var animation_tree : AnimationTree
-@export var chest_tracker : BoneAttachment3D
+@export var chest_tracker : Node3D
+@export var head_tracker : BoneAttachment3D
 @export var camera_spine : Node3D
 var chest_angle : float = 0
+
 func chest_point_at(r_position):
 	camera_point.global_position = r_position
+	camera_spine.global_position = head_tracker.global_position
 	turn_body_to_cam()
 	
 func turn_body_to_cam():
-	print(chest_tracker.rotation.y)
-	if chest_tracker.rotation.y > 0.4:
-		self.rotation.y += 0.1
-	elif chest_tracker.rotation.y < -0.6:
-		self.rotation.y -= 0.1
-
-		
+	pass
+	
 var local_vel_mag : Vector2 = Vector2.ZERO
 func walk_anim_update(velocity_magnitude : Vector2):
 	local_vel_mag = lerp(local_vel_mag, velocity_magnitude, 0.2)
---
+++ Scripts/weapon_scripts/p_2000.gd
@@ -0,0 +1,1 @@
+extends physics_item
