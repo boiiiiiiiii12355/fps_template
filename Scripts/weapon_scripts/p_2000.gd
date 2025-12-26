50729641c15d3036e2bad04a264c4ba4a703a4c0
Mel_The_Idiot <melvinngoi123@gmail.com>
2025-12-27 02:24:11 +0800

    Update DrawLine3D.gd, DrawLine3D.gd.uid, p_2000.gd, and 20 more files

--- Scripts/weapon_scripts/p_2000.gd
+++ Scripts/weapon_scripts/p_2000.gd
@@ -1,6 +1,7 @@
 extends physics_item
 @export var weapon_model : Node3D
 @export var muzzle_flash : OmniLight3D
+@export var gun_ray : RayCast3D
 var animation_player : AnimationPlayer
 
 func _ready() -> void:
@@ -18,6 +19,21 @@ func object_function():
 		animation_player.seek(0)
 		get_tree().call_group("player_animations", "play_arm_animation_from_start", "p2000_fire")
 		animation_player.play("fire")
-		muzzle_flash.omni_range = 100
+		check_hit()
+		muzzle_flash.omni_range = 1000
 		await get_tree().create_timer(0.1).timeout
 		muzzle_flash.omni_range = 0
+
+@onready var root_node : Level = owner
+var hit_particle = preload("res://particle_effects/hit_particles.tscn")
+func check_hit():
+	if gun_ray.get_collider():
+		var hit_particle_clone = hit_particle.instantiate()
+		root_node.particles.add_child(hit_particle_clone)
+		DrawLine3d.DrawLine(gun_ray.global_position, gun_ray.get_collision_point(), Color.YELLOW, 0.01)
+		hit_particle_clone.global_position = gun_ray.get_collision_point()
+		hit_particle_clone.rotation = gun_ray.get_collision_normal()
+		hit_particle_clone.restart()
+		
+	else:
+		return false
---
+++ maps/level.gd
@@ -0,0 +1,6 @@
+extends Node3D
+class_name Level
+
+#just some paths to store instances
+@export var objects : Node3D
+@export var particles : Node3D
